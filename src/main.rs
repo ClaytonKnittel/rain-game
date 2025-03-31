99deeda23a2184f671dd5f19a7ba54afba520698
Clayton Knittel <cknit1999@gmail.com>
2025-03-30 18:57:57 -0700

    Remove color/screen_object modules

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,5 @@
 #![allow(clippy::type_complexity)]
 
-mod color;
 mod framerate;
 mod gravity;
 mod movable;
@@ -8,12 +7,10 @@ mod npc;
 mod player;
 mod position;
 mod rain;
-mod screen_object;
 mod win_info;
 mod world_init;
 
 use bevy::{app::App, DefaultPlugins};
-use color::ColorPlugin;
 use framerate::FrameratePlugin;
 use gravity::GravityPlugin;
 use movable::MovePlugin;
@@ -28,7 +25,6 @@ fn main() {
     .add_plugins(DefaultPlugins)
     .add_plugins(FrameratePlugin)
     .add_plugins(WorldInitPlugin)
-    .add_plugins(ColorPlugin)
     .add_plugins((PlayerPlugin, RainPlugin, NpcPlugin))
     .add_plugins((PositionPlugin, MovePlugin, GravityPlugin))
     .run();
